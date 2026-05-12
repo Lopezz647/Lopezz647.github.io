1c84c442b475abf1a6351bff95108d18ea217e89
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-05-12 14:13:55 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <-- ESSA LINHA É OBRIGATÓRIA
-  images: {
-    unoptimized: true,
-  },
+  output: 'export', // <-- OBRIGATÓRIO para gerar a pasta 'out'
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Evita que erros de lint parem o deploy
   },
-};
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "crests.football-data.org",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
