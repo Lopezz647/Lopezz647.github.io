f383c7344bc6542f345ba208853c962309fa10a7
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-05-12 13:51:41 -0300

    Update next.config.mjs

--- Brasileirão/next.config.mjs
+++ Brasileirão/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <--- ADICIONE ESTA LINHA AQUI
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Recomendo adicionar isso também para evitar erros no deploy
   },
   images: {
     unoptimized: true,
