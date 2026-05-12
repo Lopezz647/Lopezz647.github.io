d3b1cf1f96f1a9337c858157a3dbd261592a5625
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-05-12 14:51:54 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  distDir: 'out', // Forçamos o nome da pasta de saída
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
